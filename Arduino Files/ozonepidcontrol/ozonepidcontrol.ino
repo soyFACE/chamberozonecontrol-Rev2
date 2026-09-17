23c08d6b68f56fea968bbaef5726ee9719285841
Azalea <azaleahazel@gmail.com>
2026-09-17 11:27:27 -0500

    Changed variable name formats.

--- Arduino Files/ozonepidcontrol/ozonepidcontrol.ino
+++ Arduino Files/ozonepidcontrol/ozonepidcontrol.ino
@@ -1,14 +1,14 @@
 #include "DFRobot_GP8403.h"
 DFRobot_GP8403 dac(&Wire,0x5F);
 
-String firmware_version_number = "firmware version 0002" // this will need to change with certain hardware changes. I'm not sure if I'll track it closely with most software changes
-const int OzonePin = A0;
-const int ozone_pin_alternate = A3;
-const int ballast_power_relay_pin = 12;
-const int ballast_manual_on_sense_pin = 4;
-const int ballast_auto_sense_pin = 2;
-const int bulb_intensity_manual_sense_pin = 7;
-const int bulb_intensity_auto_sense_pin = 8;
+String firmware_version_number = "firmware version 0002"; // this will need to change with certain hardware changes. I'm not sure if I'll track it closely with most software changes
+const int OZONE_PIN = A0;
+const int OZONE_PIN_ALTERNATE = A3;
+const int BALLAST_POWER_RELAY_PIN = 12;
+const int BALLAST_MANUAL_ON_SENSE_PIN = 4;
+const int BALLAST_AUTO_SENSE_PIN = 2;
+const int BULB_INTENSITY_MANUAL_SENSE_PIN = 7;
+const int BULB_INTENSITY_AUTO_SENSE_PIN = 8;
 //const int BulbPin = 9;
 const int CYCLETIME = 4000;
 float setpoint = 150;
@@ -23,10 +23,9 @@ int door_sense = 0; // this is the one of the physical door sensor
 //Find how to store these in non-volatile memory
 
 //door code
-const int door_sensor_pin ; // we can set this when we put it all together
-bool DOOR_SENSOR_NORMALLY_CLOSED = true; // we can also change this when we get the sensor
-bool DOOR_INTERLOCK_BYPASS = false; // this is the one that ignores door sensor
-int OZONE_ON_BEFORE_DOOR = 0;
+const int DOOR_SENSOR_PIN 3; // we can set this when we put it all together
+bool door_interlock_is_bypassed = false; // this is the one that ignores door sensor
+
 
 float kp = 0.0600;
 float ki = 0.0002/CYCLETIME;
@@ -38,19 +37,19 @@ float Icomponent = 1.32; // Set the initial I term to help things stabilize soon
 float Dcomponent = 0;
 float vout_in_volts;
 
-int OZONE_ON = 0;
-int RELAY_STATE = 0;
-int BALLAST_MANUAL = 0;
-int BALLAST_AUTO = 0;
-int BULB_MANUAL = 0;
-int BULB_AUTO = 0;
-int STATE = 0;
+int ozone_on = 0;
+int relay_state = 0;
+int ballast_manual = 0;
+int ballast_auto = 0;
+int bulb_manual = 0;
+int bulb_auto = 0;
+int state = 0;
 
 
 int DFRout = 0;
-int OZONEGAIN = 250;
-int CO2GAIN = 2000;
-bool FIRST_CYCLE = true;
+int ozone_gain = 250;
+int CO2_gain = 2000;
+bool first_cycle = true;
 
 
 float last_time = millis();
@@ -58,19 +57,19 @@ float this_time;
 float elapsed_time;
 
 //COMMUNICATION GLOBALS
-const byte numChars = 32;
-char receivedChars[numChars];
+const byte NUM_CHARS = 32;
+char receivedChars[NUM_CHARS];
 const char *delim = " ,:/"; 
 boolean newData = false;
 
 void setup() {
-  pinMode(ballast_power_relay_pin,OUTPUT);
-  pinMode(ballast_manual_on_sense_pin, INPUT_PULLUP);
-  pinMode(ballast_auto_sense_pin, INPUT_PULLUP);
-  pinMode(bulb_intensity_manual_sense_pin, INPUT_PULLUP);
-  pinMode(bulb_intensity_auto_sense_pin, INPUT_PULLUP);
-  pinMode(door_sensor_pin, INPUT_PULLUP);
-  digitalWrite(ballast_power_relay_pin,0);
+  pinMode(BALLAST_POWER_RELAY_PIN,OUTPUT);
+  pinMode(BALLAST_MANUAL_ON_SENSE_PIN, INPUT_PULLUP);
+  pinMode(BALLAST_AUTO_SENSE_PIN, INPUT_PULLUP);
+  pinMode(BULB_INTENSITY_MANUAL_SENSE_PIN, INPUT_PULLUP);
+  pinMode(BULB_INTENSITY_AUTO_SENSE_PIN, INPUT_PULLUP);
+  pinMode(DOOR_SENSOR_PIN, INPUT_PULLUP);
+  digitalWrite(BALLAST_POWER_RELAY_PIN,0);
   Serial.begin(9600);
   //Serial.println("<Arduino is ready>");
   while(dac.begin()!=0){
@@ -91,67 +90,50 @@ control_loop();
 recvWithStartEndMarkers();
 }
 
-bool readDoor(int rawState) {
-  return ((rawState == HIGH) == DOOR_SENSOR_NORMALLY_CLOSED))
+
+
+bool IsOzoneOn() {
+  door_sense = digitalRead(DOOR_SENSOR_PIN);
+  return (door_sense == 0) || door_interlock_is_bypassed;
 }
-
-void do_door_interock() {
-  int previous_door_sense = door_sense;
-  door_sense = readDoor(digitalRead(door_sensor_pin)) ? 1 :0
-  if (DOOR_INTERLOCK_BYPASS) {
-    return;
-  }
-  if (door_sense == 1) {
-    if (previous_door_sense == 0) {
-      OZONE_BEFORE_DOOR = OZONE_ON;
-    } 
-    OZONE_ON = 0;
-  } else if (previous_doorsense == 1) {
-    OZONE_ON = OZONE_BEFORE_DOOR;
-  }
- }
-
 
 
 void control_loop(){
   this_time = millis();
   elapsed_time = this_time - last_time;
-  
-  if(FIRST_CYCLE & elapsed_time >= CYCLETIME){
+  ozone_on = IsOzoneOn()
+  if(first_cycle & elapsed_time >= CYCLETIME){
     dac.setDACOutVoltage(0,0);
-    BALLAST_MANUAL = !digitalRead(ballast_manual_on_sense_pin);
-    BALLAST_AUTO = !digitalRead(ballast_auto_sense_pin);
-    BULB_MANUAL = !digitalRead(bulb_intensity_manual_sense_pin);
-    BULB_AUTO = !digitalRead(bulb_intensity_auto_sense_pin);
+    ballast_manual = !digitalRead(BALLAST_MANUAL_ON_SENSE_PIN);
+    ballast_auto = !digitalRead(BALLAST_AUTO_SENSE_PIN);
+    bulb_manual = !digitalRead(BULB_INTENSITY_MANUAL_SENSE_PIN);
+    bulb_auto = !digitalRead(BULB_INTENSITY_AUTO_SENSE_PIN);
     last_time = this_time;
-    do_door_interlock(); // doooooor code.
-    process_value = analogRead(OzonePin);
-    process_value = process_value/1023*OZONEGAIN;
-    process_value_alternate = analogRead(ozone_pin_alternate);
-    process_value_alternate = process_value_alternate/1023*CO2GAIN;
+    process_value = analogRead(OZONE_PIN);
+    process_value = process_value/1023*ozone_gain;
+    process_value_alternate = analogRead(OZONE_PIN_ALTERNATE);
+    process_value_alternate = process_value_alternate/1023*CO2_gain;
     error = setpoint - process_value;
     Pcomponent = error * kp * abs(error/setpoint);
-    Icomponent += error * elapsed_time * ki * OZONE_ON;
+    Icomponent += error * elapsed_time * ki * ozone_on;
     Dcomponent = (error - last_error) / elapsed_time * kd; // is there a way to make this hold the last value if the process value hasn't updated? Maybe averaging the current and last value would be easier. Matching the 4 second cycle of the monitor is probably easiest.
     last_error = error;
-    FIRST_CYCLE = false;
+    first_cycle = false;
     
     
-    }
   }
   
-  if(!FIRST_CYCLE & elapsed_time >= CYCLETIME){
+  if(!first_cycle & elapsed_time >= CYCLETIME){
     last_time = this_time;
-    do_door_interlock(); // dooooor code.
-    process_value = analogRead(OzonePin);
-    process_value = process_value/1023*OZONEGAIN;
-    process_value_alternate = analogRead(ozone_pin_alternate);
-    process_value_alternate = process_value_alternate/1023*CO2GAIN;
+    process_value = analogRead(OZONE_PIN);
+    process_value = process_value/1023*ozone_gain;
+    process_value_alternate = analogRead(OZONE_PIN_ALTERNATE);
+    process_value_alternate = process_value_alternate/1023*CO2_gain;
     
     error = setpoint - process_value;
     
     Pcomponent = error * kp * abs(error/setpoint);
-    Icomponent += error * elapsed_time * ki * OZONE_ON;
+    Icomponent += error * elapsed_time * ki * ozone_on;
     if(Icomponent >3.5){Icomponent = 3.5;}
     if(Icomponent < -.5){Icomponent = -0.5;}
     Dcomponent = (error - last_error) / elapsed_time * kd; // is there a way to make this hold the last value if the process value hasn't updated? Maybe averaging the current and last value would be easier. Matching the 4 second cycle of the monitor is probably easiest.
@@ -166,13 +148,13 @@ void control_loop(){
     if(DFRout <= 0){DFRout = 0;} //This and the line below are to help prevent instability when the DACout is near the threshhold to keep the bulb on.
     if(DFRout > 0 & DFRout < 138){DFRout = 138;} // This is the minimum DAC level that will consistently activate the UV bulb. Determined by eye.
     if(DFRout > 10000){DFRout = 10000;} // Prevent sending a value greater than 10000 (the maximum value) to the DFR.
-    digitalWrite(ballast_power_relay_pin, OZONE_ON);
-    DFRout = DFRout*OZONE_ON;
+    digitalWrite(BALLAST_POWER_RELAY_PIN, ozone_on);
+    DFRout = DFRout*ozone_on;
     dac.setDACOutVoltage(DFRout,0);
-    BALLAST_MANUAL = !digitalRead(ballast_manual_on_sense_pin);
-    BALLAST_AUTO = !digitalRead(ballast_auto_sense_pin);
-    BULB_MANUAL = !digitalRead(bulb_intensity_manual_sense_pin);
-    BULB_AUTO = !digitalRead(bulb_intensity_auto_sense_pin);
+    ballast_manual = !digitalRead(BALLAST_MANUAL_ON_SENSE_PIN);
+    ballast_auto = !digitalRead(BALLAST_AUTO_SENSE_PIN);
+    bulb_manual = !digitalRead(BULB_INTENSITY_MANUAL_SENSE_PIN);
+    bulb_auto = !digitalRead(BULB_INTENSITY_AUTO_SENSE_PIN);
 
 
     
@@ -182,8 +164,8 @@ void control_loop(){
 //    Serial.print("Elapsed_time: ");
 //    Serial.print(elapsed_time);
 //    Serial.print(",");
-    Serial.print("STATE:");
-    Serial.print(STATE);
+    Serial.print("state:");
+    Serial.print(state);
     Serial.print(",");
     Serial.print("Setpoint:");
     Serial.print(setpoint);
@@ -212,20 +194,20 @@ void control_loop(){
     Serial.print("Dcomponent:");
     Serial.print(Dcomponent,5);
     Serial.print(",");
-    Serial.print("OZONE_ON:");
-    Serial.print(OZONE_ON);
+    Serial.print("ozone_on:");
+    Serial.print(ozone_on);
     Serial.print(",");
-    Serial.print("BALLAST_MANUAL:");
-    Serial.print(BALLAST_MANUAL);
+    Serial.print("ballast_manual:");
+    Serial.print(ballast_manual);
     Serial.print(",");
-    Serial.print("BALLAST_AUTO:");
-    Serial.print(BALLAST_AUTO);
+    Serial.print("ballast_auto:");
+    Serial.print(ballast_auto);
     Serial.print(",");
-    Serial.print("BULB_MANUAL:");
-    Serial.print(BULB_MANUAL);
+    Serial.print("bulb_manual:");
+    Serial.print(bulb_manual);
     Serial.print(",");
-    Serial.print("BULB_AUTO:");
-    Serial.print(BULB_AUTO);
+    Serial.print("bulb_auto:");
+    Serial.print(bulb_auto);
     Serial.print(",");
     Serial.print("Process_Value_alternate:");
     Serial.print(process_value_alternate);
@@ -273,8 +255,8 @@ void recvWithStartEndMarkers() {
             if (input_char != endMarker) {
                 receivedChars[string_index] = input_char;
                 string_index++;
-                if (string_index >= numChars) {
-                    string_index = numChars - 1; // I should probably have this ignore the string or print an error
+                if (string_index >= NUM_CHARS) {
+                    string_index = NUM_CHARS - 1; // I should probably have this ignore the string or print an error
                 }
             }
             else {
@@ -338,17 +320,17 @@ void read_command_string(){
     case 79: //O Set Ozone Control
         token = strtok(NULL,delim);
         if(atoi(token) == 0){
-          OZONE_ON = 0;
-          //Serial.print("OZONE_ON changed to: ");
+          ozone_on = 0;
+          //Serial.print("ozone_on changed to: ");
           //Serial.println(token);
         }
         else if (atoi(token) == 1){
-          OZONE_ON = 1;
-          //Serial.print("OZONE_ON changed to: ");
+          ozone_on = 1;
+          //Serial.print("ozone_on changed to: ");
           //Serial.println(token);
         }
         else {
-           Serial.print("OZONE_ON invalid value: ");
+           Serial.print("ozone_on invalid value: ");
            Serial.println(token);
         }
         break;
